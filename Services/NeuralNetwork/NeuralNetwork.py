3b11c356e2f7853fc99c39ab89d234b990fdcddb
Alexandre THOMAS <alex1999.thomas@orange.fr>
2021-03-29 11:36:53 +0200

    Change the Yolov4 pytorch implementation

--- Services/NeuralNetwork/NeuralNetwork.py
+++ Services/NeuralNetwork/NeuralNetwork.py
@@ -1,49 +1,49 @@
+import cv2
 import torch
 
-from Services.NeuralNetwork.Utilities.configs import parse_config
-from Services.NeuralNetwork.Utilities.weights import load_weights
-
-from Services.NeuralNetwork.Utilities.devices import gpu_device_name, get_device, use_cuda
-from Services.NeuralNetwork.Utilities.images import load_image
-from Services.NeuralNetwork.Utilities.inferencing import inference_on_image
+from Services.NeuralNetwork.tool.torch_utils import do_detect
+from Services.NeuralNetwork.tool.darknet2pytorch import Darknet
 
 class NeuralNetwork:
     def __init__(self, cfg_file: str, weights_file: str, threshold: float) -> None:
         with torch.no_grad():
-            self._model = parse_config(cfg_file)
+            self._model = Darknet(cfg_file)
             
             if(self._model is None):
                 return
 
-            self._model = self._model.to(get_device())
-            self._model.eval()
+            #self._model.print_network()
+            self._model.load_weights(weights_file)
 
-            if(self._model.net_block.width != self._model.net_block.height):
-                print("Error: Width and height must match in [net]")
-                return
-
-            self._network_dim = self._model.net_block.width
-
-            print("Loading weights...")
-            load_weights(self._model, weights_file)
-
-            print("DARKNET")
-            print("GPU:", gpu_device_name())
-            print("Config:", cfg_file)
-            print("Weights:", weights_file)
-            print("Version:", self._model.version)
-            print("Images seen:", self._model.imgs_seen)
-            print("")
-            print("Network Dim:", self._network_dim)
+            self._model = self._model.to('cuda:0' if torch.cuda.is_available() else 'cpu')
 
             self._obj_thresh = threshold
 
     def process(self, image_file: str):
         with torch.no_grad():
-            image = load_image(image_file)
-            if(image is None):
+            img = cv2.imread(image_file)
+            sized = cv2.resize(img, (self._model.width, self._model.height))
+            sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+            if(img is None):
                 return
 
-            detections = inference_on_image(self._model, image, self._network_dim, self._obj_thresh)
+            self._model.eval()
+            device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+            boxes = do_detect(self._model, sized, self._obj_thresh, 0.4, device)
+
+            detections = []
+            img_height, img_width = img.shape[:2]
+            for box in boxes[0]:
+                x1 = int(box[0] * img_width)
+                y1 = int(box[1] * img_height)
+                x2 = int(box[2] * img_width)
+                y2 = int(box[3] * img_height)
+
+                xywh = (x1, y1, x2-x1, y2-y1)
+                conf = box[5]
+                id = box[6]
+
+                detections.append([xywh, id, conf])
 
             return detections
