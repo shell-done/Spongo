38c9826cc3532e8027b6fcad755f55953211a812
Alexandre THOMAS <alex1999.thomas@orange.fr>
2021-04-05 17:54:05 +0200

    Fix wrong zoom

--- Components/Download/PreviewComponent.py
+++ Components/Download/PreviewComponent.py
@@ -17,7 +17,6 @@ class PreviewComponent(QGroupBox):
 
         self._web_view = QWebEngineView(self)
         self._web_view.setContextMenuPolicy(Qt.NoContextMenu)
-        self._web_view.setZoomFactor(0.5)
 
         web_view_container = StylizableWidget()
         web_view_container.setObjectName("WebView")
@@ -31,5 +30,5 @@ class PreviewComponent(QGroupBox):
     def update(self, report_writer: ReportWriter):
         html = report_writer.toHTML()
         self._web_view.setHtml(html, QUrl("qrc:/"))
-        self._web_view.setZoomFactor(1)
+        self._web_view.setZoomFactor(0.5)
     
